6bc0a580dc956e70c855c46de9423424bbb49825
HenriqueEberhardt <159157371+HenriqueEberhardt@users.noreply.github.com>
2024-07-03 15:09:10 -0300

    Update TesteGitEclipse.java

--- IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
+++ IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
@@ -4,6 +4,7 @@ public class TesteGitEclipse {
 
 	public static void main(String[] args) {
 		System.out.println("Integração Git Eclipse Primeiro Teste");
+		System.out.println("Alterando para o eclipse");
 
 	}
 
