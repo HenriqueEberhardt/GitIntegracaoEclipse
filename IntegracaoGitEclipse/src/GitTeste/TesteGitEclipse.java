550a9607a6e8770bf384d9e64f1ac2a15387b19b
HenriqueEberhardt <hen.eberhardt@gmail.com>
2024-07-03 15:15:50 -0300

    opa maninho

--- IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
+++ IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
@@ -5,7 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 		System.out.println("Integração Git Eclipse Primeiro Teste");
 		System.out.println("Alterando para o eclipse");
-
+		System.out.println("Eu como cocô aos sabados");
 	}
 
 }
